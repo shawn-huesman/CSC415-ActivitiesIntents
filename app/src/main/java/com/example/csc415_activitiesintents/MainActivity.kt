e3cfac962fa4193d5a068c6d0bad015b3065eed0
shawn-huesman <mrshawnhuesman@gmail.com>
2021-10-31 16:59:46 -0400

    Completed Tasks 1+2 - Created second activity and intent to launch activity

--- app/src/main/java/com/example/csc415_activitiesintents/MainActivity.kt
+++ app/src/main/java/com/example/csc415_activitiesintents/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.csc415_activitiesintents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.content.Intent
+
+
+
 
 class MainActivity : AppCompatActivity() {
+    private val LOG_TAG = MainActivity::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun launchSecondActivity(view: android.view.View) {
+        Log.d(LOG_TAG, "Button clicked!")
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
