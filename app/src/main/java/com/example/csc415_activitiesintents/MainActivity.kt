8a96890bd3ac9afae5a3933584692c1e02c7513f
shawn-huesman <mrshawnhuesman@gmail.com>
2021-10-31 20:33:50 -0400

    Completed Tasks 3+4 - Send and return data from main activity to second activity

--- app/src/main/java/com/example/csc415_activitiesintents/MainActivity.kt
+++ app/src/main/java/com/example/csc415_activitiesintents/MainActivity.kt
@@ -1,24 +1,52 @@
 package com.example.csc415_activitiesintents
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.content.Intent
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
-
-
+const val EXTRA_MESSAGE = "example message"
+val TEXT_REQUEST = 1
 
 class MainActivity : AppCompatActivity() {
     private val LOG_TAG = MainActivity::class.java.simpleName
+    private lateinit var mMessageEditText: EditText
+    private lateinit var mReplyHeadTextView: TextView
+    private lateinit var mReplyTextView: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mMessageEditText = findViewById(R.id.editText_main);
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
+
     }
 
-    fun launchSecondActivity(view: android.view.View) {
+    fun launchSecondActivity(view: View) {
         Log.d(LOG_TAG, "Button clicked!")
         val intent = Intent(this, SecondActivity::class.java)
-        startActivity(intent)
+        intent.putExtra(EXTRA_MESSAGE, mMessageEditText.text.toString());
+        startActivityForResult(intent, TEXT_REQUEST)
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?)
+    {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == TEXT_REQUEST)
+        {
+            if (resultCode == RESULT_OK)
+            {
+                mReplyHeadTextView.visibility = View.VISIBLE
+                mReplyTextView.text = data!!.getStringExtra(EXTRA_REPLY)
+                mReplyTextView.visibility = View.VISIBLE
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/csc415_activitiesintents/SecondActivity.kt
+++ app/src/main/java/com/example/csc415_activitiesintents/SecondActivity.kt
@@ -1,12 +1,35 @@
 package com.example.csc415_activitiesintents
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+val EXTRA_REPLY: String = "com.example.android.CSC415-ActivitiesIntents.extra.REPLY"
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var mReply: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val intent: Intent = getIntent()
+        val message: String? = intent.getStringExtra(EXTRA_MESSAGE)
+        val textView: TextView = findViewById(R.id.text_message)
+        textView.setText(message)
+        mReply = findViewById(R.id.editText_second)
     }
+
+    fun returnReply(view: View?) {
+        val replyIntent = Intent()
+        replyIntent.putExtra(EXTRA_REPLY, mReply.text.toString())
+        setResult(RESULT_OK, replyIntent)
+        finish()
+    }
+
 }
